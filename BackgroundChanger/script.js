c6bf84667acd4b1ca53bad967c789e126df536e3
Altamash Shaikh <altamashshaikh0017@gmail.com>
2024-08-28 15:57:55 +0530

    Add Background Change Project

---
+++ BackgroundChanger/script.js
@@ -0,0 +1,54 @@
+// const randomColor=function(){
+//     const hex="0123456789ABCDEF";
+//     let color= "#";
+  
+//     for(let i=0; i<6; i++){
+//       color += hex[Math.floor(Math.random()*16)];
+//       console.log(color);
+//     }
+//     return color;
+// }
+
+// let intervalId;
+
+// const startChangingColor=function(){ 
+//     if(!intervalId){   //intervalId==null
+//         intervalId=setInterval(changeBgColor,1000);
+//     }
+// }
+// function changeBgColor(){
+//     document.body.style.backgroundColor = randomColor();
+//  }
+// const stopChangingColor=function(){
+//   clearInterval(intervalId);
+//   intervalId=null;
+// }
+// document.querySelector('#start').addEventListener('click',startChangingColor);
+// document.querySelector('#stop').addEventListener('click',stopChangingColor);
+
+const randomColor = function () {
+    let hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  console.log(randomColor());
+  let intervalId;
+  const startChangingColor = function () {
+    if (!intervalId) {
+      intervalId = setInterval(changeBg, 1000);
+    }
+  };
+  const changeBg = function () {
+    document.body.style.backgroundColor = randomColor();
+  };
+  const stopChangingColor = function () {
+    clearInterval(intervalId)
+    intervalId = null;
+  };
+  document.querySelector('#start').addEventListener('click', startChangingColor);
+  document.querySelector('#stop').addEventListener('click', stopChangingColor);
+  
+  
